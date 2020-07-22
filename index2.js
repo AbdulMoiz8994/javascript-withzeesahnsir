67041308b698ee1bc2c9e6db128a4c3c26c13ed0
AbdulMoiz8994 <63612568+AbdulMoiz8994@users.noreply.github.com>
2020-07-22 17:15:25 +0500

    add changes

---
+++ index2.js
@@ -0,0 +1,29 @@
+// var a;
+// console.log(a);
+// a = "abdu moiz helo world";
+// console.log(a);
+// a = undefined;
+// console.log(a);
+// var  a = null;
+// console.log(a);
+// a = 35;
+// console.log(a);
+// a = null;
+// console.log(a);
+// var name = "moizkz";
+// console.log(name);
+// name = 25;
+// console.log(name);
+// name = true;
+// console.log(name);
+var nationality = "Pakistan";
+var name = "abdul moiz";
+var isfeePaid = false;
+var age = 25;
+
+console.log(typeof age);
+console.log(typeof nationality);
+console.log(typeof name);
+console.log(typeof isfeePaid);
+
+isfeePaid = 45; console.log(typeof isfeePaid);
