8fd651b85e7f006fc4038a17bab32379a655c294
AbdulMoiz8994 <63612568+AbdulMoiz8994@users.noreply.github.com>
2020-07-24 12:44:04 +0500

    index1.html

--- index1.js
+++ index1.js
@@ -1,21 +1,25 @@
-var age = 20;
-var name = "abdul moiz";
-var nationality = "Pakistani";
-var feePaid = false
-var a;
-a = "khanzada"
-var a;
-a = 40;
-var a;
-a = "India";
+// var age = 20;
+// var name = "abdul moiz";
+// var nationality = "Pakistani";
+// var feePaid = false
+// var a;
+// a = "khanzada"
+// var a;
+// a = 40;
+// var a;
+// a = "India";
 
+// console.log(age);
+// age = 40;
+// console.log(age)
+// console.log(name);
+// name = "kahnzada"
+// console.log(name);
+// console.log(nationality);
+// nationality = "India";
+// console.log(nationality);
+// console.log(feePaid);
+var age = prompt ("what is your age?");
 console.log(age);
-age = 40;
-console.log(age)
+var name = prompt("what is your name");
 console.log(name);
-name = "kahnzada"
-console.log(name);
-console.log(nationality);
-nationality = "India";
-console.log(nationality);
-console.log(feePaid);
