8ea94d40f9031c94b53ec52429899aab015e532d
AbdulMoiz8994 <63612568+AbdulMoiz8994@users.noreply.github.com>
2020-07-26 10:23:16 +0500

    add index6.js

---
+++ index6.js
@@ -0,0 +1,14 @@
+// var name = prompt("what is your name?" , "Abdul moiz")
+// console.log(name);
+// var age = prompt("what is your age?");
+// console.log(age);
+// var age = prompt("what is your age?");
+// console.log(age);
+// var newAge = 5 + age;
+// console.log(newAge);
+var a = 6; 
+var b = 7;
+var c = a + b;
+console.log(a);
+console.log(b);
+console.log(c);
