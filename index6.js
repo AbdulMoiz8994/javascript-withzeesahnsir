bfd023ed5eee2a6da7663ce1ca0520ed402680e6
AbdulMoiz8994 <63612568+AbdulMoiz8994@users.noreply.github.com>
2020-07-26 11:30:06 +0500

    update index6.js

--- index6.js
+++ index6.js
@@ -1,14 +1,20 @@
-// var name = prompt("what is your name?" , "Abdul moiz")
-// console.log(name);
-// var age = prompt("what is your age?");
-// console.log(age);
-// var age = prompt("what is your age?");
-// console.log(age);
-// var newAge = 5 + age;
-// console.log(newAge);
+var name = prompt("what is your name?" , "Abdul moiz")
+console.log(name);
+var age = prompt("what is your age?");
+console.log(age);
+var age = prompt("what is your age?");
+console.log(age);
+var newAge = 5 + age;
+console.log(newAge);
 var a = 6; 
-var b = 7;
-var c = a + b;
-console.log(a);
-console.log(b);
-console.log(c);
+var b = prompt("what is your age?");
+var c = Number(b);
+var d = a + c;
+console.log(c)
+console.log(d);
+console.log(Number(true));
+var a = 10;
+var b = prompt("what is your name?");
+var c = parseFloat(b);
+var d = a + c;
+console.log(d);
