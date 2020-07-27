5812c1221c034d03a8a9deddcb1f14de69f3c52f
AbdulMoiz8994 <63612568+AbdulMoiz8994@users.noreply.github.com>
2020-07-27 12:42:20 +0500

    update undex7.js

--- index7.js
+++ index7.js
@@ -8,10 +8,39 @@
 
 // var c = a + parseInt(b);
 // console.log(c);
-var a = 8
-var b = prompt("what is your age?");
-var c = Number(b);
-var d = a * c;
-console.log(d);
+// var a = 8
+// var b = prompt("what is your age?");
+// var c = Number(b);
+// var d = a * c;
+// console.log(d);
 
-console.log(Number("7 7"));
+// console.log(Number("7 7"));
+// var a = 5;
+// var b = 5;
+
+// console.log(a == b);
+// console.log(a != b);
+// console.log(a > b);
+// console.log(a < b);
+// console.log(a >= b);
+// console.log(a <= b);
+// var a = 8;
+// var b = 9;
+
+// console.log(a == b);
+// console.log(a != b);
+// console.log(a > b);
+// console.log(a < b);
+// console.log(a >= b);
+// console.log(a <= b);
+
+var a = 10;
+var b = "10";
+
+console.log(a == b);
+console.log(a === b);
+var a = 10;
+var b = "10";
+console.log(a == b);
+console.log(a === b);
+var a
