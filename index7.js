1c36169ee68c765b6ae57f18ab9784ee54ccc273
AbdulMoiz8994 <63612568+AbdulMoiz8994@users.noreply.github.com>
2020-07-27 12:03:31 +0500

    add index7.js

---
+++ index7.js
@@ -0,0 +1,17 @@
+// var a = 6;
+// var b = "8";
+
+// var c = a + b;
+// console.log(c);
+// var a = 6;
+// var b = "8";
+
+// var c = a + parseInt(b);
+// console.log(c);
+var a = 8
+var b = prompt("what is your age?");
+var c = Number(b);
+var d = a * c;
+console.log(d);
+
+console.log(Number("7 7"));
