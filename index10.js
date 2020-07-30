d16c1f48a0047c482c8e1f3924f30587efb62f7b
AbdulMoiz8994 <63612568+AbdulMoiz8994@users.noreply.github.com>
2020-07-30 14:23:37 +0500

    add index.js

---
+++ index10.js
@@ -0,0 +1,162 @@
+// var a = 5;
+// var b = prompt("what is your name?", "abdul moiz");
+// var c = b;
+// console.log(c);
+// var a = 7;
+// var b = 13.55;
+// var c = parseInt(b);
+// var d = a + c;
+// console.log(d);
+// var a = 7;
+// var b = 13.55;
+// var c = parseFloat(b);
+// var d = a + c;
+// console.log(d);
+// var a = 7;
+// var b = 15.65;
+// var c = a * Number(b);
+// console.log(c);
+// var age = prompt("what is your age?");
+// var age1 = 7;
+// var age1 = parseInt(age) + age1;
+// console.log(age1);
+// var a = Number(true);
+// console.log(a);
+// var b = Number(false);
+// console.log(b);
+// var a = 5;
+// var b = prompt("what is your score");
+// var c = a + parseInt(b);
+// console.log(c);
+// var a = 6;
+// console.log(a == 6);
+// console.log(a === 6);
+// console.log(a != 6);
+// console.log(a > 6);
+// console.log(a < 6);
+// console.log(a >= 6);
+// console.log(a <= 6);
+// var a = 7;
+// var b = 7;
+// console.log(a == b);
+// console.log(a === b);
+// console.log(a != b);
+// console.log(a > b);
+// console.log(a < b);
+// console.log(a >= b);
+// console.log(a <= b);
+// var a = 5;
+// var b = "5";
+// console.log(a ==b);
+// console.log(a===b);
+// var a=60;
+// var b = a >= 60 && a < 100;
+// console.log(b);
+// var a=73;
+// var b=70;
+// console.log(a < 70 && b <= 100 && b!= a);
+// var a = 60;
+// var b = 50;
+// console.log(a<b || a > b7);
+// var a=70;
+// var b=80;
+// var c = a > b || b < 100 || a !=b;
+// console.log(c); 
+//var a =60
+//console.log(a<50 || a >70);
+// var a= 60;
+// console.log(!a);
+// var a= 50;
+// var b =!(a > 50);
+// console.log(b);
+// var c =60
+// var d = 70;
+// var e = !(c > d || c < 70);
+// console.log(e);
+// var a= 5;
+// var b=4;
+// var c = !(a > b || a >= b);
+// console.log(c);
+// var d = !!(c);
+// console.log(d);
+// var a = 1
+// var b = !(a);
+// var c= !!(a);
+// console.log("A "+a)
+// console.log("B "+b);
+// console.log("C "+c);
+// var score = 60;
+// var a = score > 50 && score < 70 && score == 66;
+// console.log(a);
+// var Score = prompt("what is your Score");
+// Score = parseInt(Score);
+// if (Score >= 80){
+//     console.log("A Grade");
+// }
+// else if (Score >= 70){
+//     console.log("B Grade");
+// }
+// else if(Score >= 60){
+//     console.log("C Grade");
+// }
+// else if(Score >= 55){
+//     console.log("D Grade");
+// }
+// else{
+//     console.log("Fail");
+// }
+// var Score = prompt("what is your Score");
+// Score = parseInt(Score);
+// if (Score >= 80){
+//     console.log("A Grade");
+//     if (Score >= 90){
+//         console.log("Reward $100");
+//     }
+//     else{
+//         console.log("Reward $50");
+//     }
+// }
+// else if (Score >= 70){
+//     console.log("B Grade");
+//     if(Score >=70){
+//         console.log("Reward $10")
+//     }
+// }
+// else if(Score >= 60){
+//     console.log("C Grade");
+// }
+// else if(Score >= 55){
+//     console.log("D Grade");
+// }
+// else{
+//     console.log("Fail");
+// }
+// var age = prompt("what is your age");
+// age = parseInt(age);
+// if (age < 60){
+//     alert("Allow");
+//     if(age < 18){
+//         alert("Allow but some restriction")
+//     }
+// }
+// else{
+//     alert("Not Allow")
+// }
+var score = 69.99;
+if(score >= 80 && score <= 100){
+    console.log("Grade A");
+}
+else if(score >= 70 && score <=79.99){
+    console.log("Grade B")
+}
+else if (score >= 60 && score <=69.99){
+    console.log("Grade C");
+}
+var input = "M";
+if(input === "A" ||  input === "E" || input || "I" || input === "O" || input == "U"){
+    console.log("IT is a vowel");
+}
+else{
+    console.log("It is an constant");
+}
+
