6929a2c784f64fb12d9864748be3f3e20833532f
AbdulMoiz8994 <63612568+AbdulMoiz8994@users.noreply.github.com>
2020-07-31 16:04:56 +0500

    add index11.js

---
+++ index11.js
@@ -0,0 +1,12 @@
+// var a = 6;
+// var b = prompt("what is your score");
+// var c = parseInt(b);
+// console.log(a + c);
+// var a = prompt("what is your number");
+// var b = parseFloat(2);
+// console.log(a *b);
+var a = 5;
+var b=prompt("what is your score")
+var c = Number(b);
+console.log(a+c);
+console.log(a*c);
