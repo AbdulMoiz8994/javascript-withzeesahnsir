08a9aa69c434e6349692ec73bb64b759fd6ee77e
AbdulMoiz8994 <63612568+AbdulMoiz8994@users.noreply.github.com>
2020-08-03 12:42:54 +0500

    update index12.js

--- index12.js
+++ index12.js
@@ -27,33 +27,84 @@
 // var a = 50;
 // var b = 39;
 // console.log(a<b && b>a || b>a);
-var a = 10;
-var b =11;
-var c = a>b || a<b || a===b;
-console.log(c);  
-var age = prompt("what is your age");
-age = Number(age);
-if(age >=50 && age <=80){
-    alert("Not Allow");
-}
-    // if (age  >80){
-    //     console.log("Say Sorry");
-    //     alert("Say Sorry");
+// var a = 10;
+// var b =11;
+// var c = a>b || a<b || a===b;
+// console.log(c);  
+// var age = prompt("what is your age");
+// age = Number(age);
+// if(age >=50 && age <=80){
+//     alert("Not Allow");
+// }
+//     // if (age  >80){
+//     //     console.log("Say Sorry");
+//     //     alert("Say Sorry");
+//     // }
+// else if(age <= 18){
+//     alert("Allowed but Some restrictions");
+// }
+// else if(age >18 && age <50){
+//     console.log("Allow Fully");
+//     alert("Allow Fully");
+//     if(age >18 && age <50){
+//         alert("Thank you for Shopping");
+//     }
+// }
+    // else{
+    //     alert("Share with their Friends");
+    //     alert("Sorry");
     // }
-else if(age <= 18){
-    alert("Allowed but Some restrictions");
-}
-else if(age >18 && age <50){
-    console.log("Allow Fully");
-    alert("Allow Fully");
-    if(age >18 && age <50){
-        alert("Thank you for Shopping");
-    }
-}
-    else{
-        alert("Share with their Friends");
-        alert("Sorry");
-    }
 // else{
 //     alert("Error");
 // }
+// console.log("hello JavaScript");
+// console.log(27+18+20-30);
+// console.log("45");
+// document.write("hello moiz");
+// document.write(20+10);
+// document.write("20+10");
+// var name = "Abdul moiz";
+// console.log(name);
+// alert(name);
+// var name = prompt("what is your name?");
+// var age = 45;
+// var name = "Abdul moiz";
+// var nationality = "Pakistan";
+// var isfeePaid = prompt("is feePaid");
+// if(isfeePaid = true){
+//     console.log("Thanks");
+//     alert("Thanks");
+// }
+// else if(isfeePaid =false){
+//     alert("false");
+// }
+// console.log("name = "+age);
+// console.log("nationality = "+nationality);
+// console.log("name = "+name);
+// // console.log("isfeePaid = "+isfeePaid);
+// var nationality;
+// nationality = "Pakistan";
+// console.log(nationality);
+// nationality = "india";
+// console.log(nationality);
+// var a =13;
+// var b ="45";
+// var c = "Abdul moiz";
+// var d = "khanz";
+// var e = true;
+// var age;
+// console.log(age);
+// var f = null;
+// console.log(f);
+// console.log(typeof a);
+// console.log(typeof b);
+// console.log(typeof c);
+// console.log(typeof d);
+// console.log(typeof e);
+// console.log(typeof a);
+var age = 34;
+console.log(age);
+age = "hello";
+console.log(age);
+age = true;
+console.log(age);
