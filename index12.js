4226fb003f388b887924ac512cc5f2d3eb8e7f4b
AbdulMoiz8994 <63612568+AbdulMoiz8994@users.noreply.github.com>
2020-08-03 09:52:53 +0500

    update index12.js

--- index12.js
+++ index12.js
@@ -31,3 +31,29 @@ var a = 10;
 var b =11;
 var c = a>b || a<b || a===b;
 console.log(c);  
+var age = prompt("what is your age");
+age = Number(age);
+if(age >=50 && age <=80){
+    alert("Not Allow");
+}
+    // if (age  >80){
+    //     console.log("Say Sorry");
+    //     alert("Say Sorry");
+    // }
+else if(age <= 18){
+    alert("Allowed but Some restrictions");
+}
+else if(age >18 && age <50){
+    console.log("Allow Fully");
+    alert("Allow Fully");
+    if(age >18 && age <50){
+        alert("Thank you for Shopping");
+    }
+}
+    else{
+        alert("Share with their Friends");
+        alert("Sorry");
+    }
+// else{
+//     alert("Error");
+// }
