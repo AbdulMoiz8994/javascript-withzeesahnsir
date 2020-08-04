0c589d2ea5b6e1216211782cdf5d7c6df8b66551
AbdulMoiz8994 <63612568+AbdulMoiz8994@users.noreply.github.com>
2020-08-04 13:00:48 +0500

    add index13.js

---
+++ index13.js
@@ -0,0 +1,103 @@
+// alert("hello JavaScript");
+// var name ="abdul moiz";
+// console.log(name);
+// var name = "abdul moiz";
+// var age = 24;
+// var nationality = "Pakistan";
+// var isfeepaid = true;
+// console.log(name);
+// console.log(age);
+// console.log(nationality);
+// console.log(isfeepaid);
+// var name = prompt("what is your name");
+// var age = prompt("what is your age");
+// age = parseInt(age);
+// var nationality = prompt("what is your nationalty")
+// var isfeepaid = prompt("is fee pay");
+// if(isfeepaid == true){
+//     alert("Thanks");
+// }
+// else if(isfeepaid == false){
+//     alert("you must pay the fee");
+// }
+// var user ="abdul moiz";
+// console.log(user);
+// var _for = 10;
+// console.log(_for);
+// var $while = "$1000";
+// console.log($while);
+// var rose = "beautiful";
+// console.log(rose);
+// var fullName = "Abdul moiz";
+// console.log(fullName);
+// var a = 5;
+// var b =10;
+// var c = a+b;
+// console.log(c);
+// var a = 5;
+// var b =10;
+// console.log(a+b);
+// var a =10
+// a *= 5;
+//  b*= 19
+// console.log(a);
+// var b = 20;
+// b += 10;
+// console.log(b);
+// var a = 5;
+// a /= 10;
+// console.log(a);
+// var a = 10+2 /(5+4) * 10-4;
+// console.log(a);
+// var b = 5*(3+9);
+// console.log(b);
+// var a =6;
+// a++;
+// console.log(a);
+// var a =6;
+// console.log(++a);
+// var a =10
+// ++a;
+// a++;
+// --a;
+// a--;
+// console.log(a); 
+// var a = 10;
+// var b = a++;
+// var c = a+b;
+// console.log(a);
+// console.log(b);
+// console.log(c);
+// var a = 10
+// var b = ++a;
+// console.log(a);
+// console.log(b);
+// var a =6;
+// var b = a--;
+// console.log(a);
+// console.log(b);
+// var a = 6;
+// var b =10;
+// var c = ++a - --b + a++;
+// console.log(a);
+// console.log(b);
+// console.log(c);
+// var a= 4;
+// var b=3;
+// var c = a++ + --b - --a;
+// // 4 + 2 - 4
+// console.log(a);
+// console.log(b);
+// console.log(c);
+var  a = "6" + 2
+console.log(a);
+var b = 3 + 4
+console.log(b);
+var c = "hello " + 2;
+console.log(c);
+var d = "hello " + 3 + 3;
+console.log(d);
+var e = 3 + 2 + " hello";
+console.log(e); 
+var f = "hello " + (10+6);
+console.log(f);
