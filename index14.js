e607024e7e9e807fb76b574a8823ab771246df44
AbdulMoiz8994 <63612568+AbdulMoiz8994@users.noreply.github.com>
2020-08-05 18:23:23 +0500

    add index14.js

---
+++ index14.js
@@ -0,0 +1,125 @@
+// var score = prompt("what is your score");
+// score = Number(score);
+// if(score >= 80 && score <=100){
+//     console.log("Grade A");
+// }
+// else if(score >=70 && score <=79.99){
+//     console.log("Grade B");
+// }
+// else if(score >=60 && score <=69.99){
+//     console.log("Grade C");
+// }
+// else{
+//     console.log("Fail");
+// }
+// var input = prompt("The fees has Paid?");
+// if(input == "true" || input == "yes"){
+//     alert("Thanks")
+// }
+// else{
+//     console.log("fees should be Paid on time");
+//     alert("fees should be Paid on time");
+
+// }
+// var a1 = "cat" && "Dog";
+// var a2 = false && "Dog";
+// var a3 = "cat" && false;
+// var a4 = "cat" && true;
+// var a5 = false && "";
+// var a6 = '' && false;
+// var a7 = 0 && 1;
+// var a8 = "0" && 1;
+// var a9 = 1 && false
+// var a10 = true && {};
+
+// console.log(a1);
+// console.log(a2);
+// console.log(a3);
+// console.log(a4);
+// console.log(a5);
+// console.log(a6);
+// console.log(a7);
+// console.log(a8);
+// console.log(a9);
+// console.log(a10);
+// var a12 = false && {};
+// console.log(a12);
+// var a13 = "cat" && (3==4);
+// console.log(a13);
+// var a14 = 'cat' && 0;
+// console.log(a14);
+// var a15 = "cat" && 10;
+// console.log(a15);
+// var a16 = 0 && "Dog";
+// console.log(a16);
+// var a17 = undefined && 'cat';
+// console.log(a17);
+// var a1 = "cat" || "Dog";
+// console.log(a1); 
+// var a2 = false || "cat";
+// console.log(a2);
+// var a2 = false || "true";
+// console.log(a2);
+// var a3 = false || true;
+// console.log(a3);
+// var a2 = '' || "false";
+// console.log(a2);
+// var a2 = "" || false;
+// console.log(a2);
+// var a2 = false || "";
+// console.log(a2);
+// var a2 = 0 || 1;
+// console.log(a2);
+// var a2 = 10 || false ;
+// console.log(a2);
+// var a2 = false || {};
+// console.log(a2);
+// var a2 = true || {};
+// console.log(a2);
+// var a2 = "cat" || (3 == 5);
+// console.log(a2);
+// var a2 = (3===5) || "cat";
+// console.log(a2);
+// var a2 = undefined || "cat";
+// console.log(a2);
+var a = 5;
+var b = 0;
+var c = a || b;
+console.log(c);
+var a = 5;
+var b = 6;
+console.log(a && b);
+var a = 5;
+var b = null;
+console.log(a && b);
+var a = 10;
+var b = 0;
+console.log(a && b);
+var a = 5;
+var b = 0;
+var c = true && b && "cat";
+console.log("c = "+c);
+var a = 5;
+var b = 0;
+var c = true || b || "cat";
+console.log(c);
+var a = 5;
+var b = 0;
+var c = "" || b || "cat";
+console.log(c);
+var a = 5;
+var b = 0;
+var c = '' || "dog" || 0;
+console.log(c);
+var a = 5;
+var b = 0;
+var c = true && {};
+console.log("c = "+c);
+var a = 5;
+var b = 0;
+var c = false ||0;
+console.log("c = "+c);
+
+
+
+
