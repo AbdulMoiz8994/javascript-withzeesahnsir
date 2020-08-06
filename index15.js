3a6a7d77cc4358fad226b8b03f5961006bc0f98b
AbdulMoiz8994 <63612568+AbdulMoiz8994@users.noreply.github.com>
2020-08-06 14:17:20 +0500

    add index15.js

---
+++ index15.js
@@ -0,0 +1,79 @@
+// var a = 10;
+// var b = 15;
+// console.log(a + b);
+// var name = prompt("what is your name");
+// var age = prompt("what is your age");
+// age = Number(age);
+// var email = prompt("Email");
+// var isfeePaid = prompt("have you apid the fees");
+// console.log(name);
+// console.log(age);
+// console.log(email);
+// console.log(isfeePaid);
+// console.log(typeof name);
+// console.log(typeof age);
+// console.log(typeof email);
+// console.log(typeof isfeePaid);
+// document.write("khanzada123");
+// var a;
+// console.log(a);
+// var age = 56;
+// console.log(age);
+// var count = 80;
+// var count = 75;
+// console.log(count);
+// var a = "hello world";
+// console.log(a)
+// var a = 5;
+// var b = 7;
+// var c = 0;
+// c = a+b;
+// console.log(c);
+// var fullName = "Abdul moiz";
+// console.log(fullName);
+// var _xyz = "khanzada";
+// console.log(_xyz);
+// var $name = "zafar Ali"
+// console.log($name);
+// var age1 = 45;
+// console.log(age1);
+// var rose = "hello"
+// console.log(rose);
+// var a = 6;
+// a = a+4;
+// console.log(a);
+// var b = 10;
+// b = a*10;
+// console.log(b);
+// var a = 5;
+// a += 20;
+// console.log(a);
+// var b =20;
+// b -= 30;
+// console.log(b);
+// b *=30;
+// // b = 10*(-30);
+// console.log(b);
+// a = 6;
+// a /=3;
+// console.log(a);
+// var a = 4;
+// var b =9;
+// var c=10;
+// var d = (a/c) + a*12 - b -(3 + b);
+// console.log(d);
+// var a = 50;
+// a = ++a;
+// console.log(a);
+// a =20;
+// console.log(a++);
+// console.log(--a)
+// console.log(a--)
+// console.log(a++)
+var a= 10;
+var b = ++a;
+var c = a++ - 5 + --b + --a;
+
+console.log(a);
+console.log(b);
+console.log(c);
