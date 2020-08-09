efb9205ad61070f6a6a6437eeb0bb1994b4d3c5e
AbdulMoiz8994 <63612568+AbdulMoiz8994@users.noreply.github.com>
2020-08-09 11:58:40 +0500

    add index16.js

--- index16.js
+++ index16.js
@@ -1,6 +1,37 @@
-var name = prompt("whtat is your name?");
-console.log(name);
-var age = 34;
-console.log(age);
-var email = prompt("what is your email")
-console.log(email);
+// var fullName = "Abdul moiz";
+// const uid = "abdul moiz";
+// console.log(uid);
+// var fullName = "abdul moiz";
+// var email = "moiza8994@gmail.com";
+// var password = "123456";
+// var confirmpassword = "123456";
+// var courseCount = 0;
+// var isloggedInFromGoogle = false;
+ 
+// fullName = prompt("what is your name");
+// console.log("Full Name: "+fullName);
+// console.log("Email is: "+email);
+// console.log(`
+//    user is : ${fullName}
+//    and his email is : ${email}
+//    and user the passwod : ${password}
+//    and coursecount of user is : ${courseCount}
+// `);
+var num1 = 6;
+var num2 = 7;
+console.log(num1 * num2);
+var answer = num1 > num2;
+console.log(answer);
+// D = (l - s)/l * 100
+// a + b * c / d *e; this is wrong method not best practice
+// a + ((b * c) / d) - e this is best parctice to do in brekits which action perfrom first
+var sellingPrice = 199;
+var listingPrice = 799;
+var discountPercent = ((listingPrice - sellingPrice)/ listingPrice) * 100;
+console.log("Discount Percentage is: "+discountPercent);
+// math libray we will use below
+displayDiscountPercentage = Math.round(discountPercent);
+console.log(displayDiscountPercentage + "% OFF");
+var result = listingPrice > sellingPrice;
+console.log(typeof result);
+console.log(result);
