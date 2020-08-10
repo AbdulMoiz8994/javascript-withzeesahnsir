bc93b52ad16ca654458daad8accbcf83a280a12c
AbdulMoiz8994 <63612568+AbdulMoiz8994@users.noreply.github.com>
2020-08-10 17:01:14 +0500

    add index17.js

---
+++ index17.js
@@ -0,0 +1,70 @@
+// var fullName = "abdul moiz";
+// var fatherName = "zafar Ali";
+// var email = "moiza8994@gmail.com";
+// var password = 123455;
+// var confirmPassword = 123455;
+// var country = "Pakistan";
+// var state = "Sindh";
+// var nationality = "Pakistani";
+// console.log(`
+//      Full Name : ${fullName}
+//      Father Name : ${fatherName}
+//      Email : ${email}
+//      Password : ${password}
+//      Confirm Password : ${confirmPassword}
+//      Country : ${country}
+//      state : ${state}
+//      Nationality : ${nationality}
+// `)
+// var name = "abdul moiz attari"
+// var age = 18;
+// console.log(name);
+// console.log(age+ " Greater");
+
+// var a = 7;
+// var b = 8;
+// var c = a + b;
+// console.log(c);
+// var a = 7;
+// var b = 8;
+// var c = a - b;
+// console.log(c);
+// var a = 7;
+// var b = 8;
+// var c = a * b;
+// console.log(c);
+// var a = 7;
+// var b = 8;
+// var c = a / b;
+// console.log(c);
+// value = Math.round(c);
+// console.log(value+ "%");
+// var a = 5;
+// var b = a++;
+// var c = ++a;
+// console.log(a);
+// console.log(b);
+// console.log(c);
+// var a =8;
+// var b =5;
+// var c = ++a - b++ + --a + b++;
+// console.log(a);
+// console.log(b);
+// console.log(c);
+// var a =9;
+// var b =17;
+// console.log(a);
+// console.log(b);
+// console.log(a++ + --b + --b - --a - a--);
+// var name = prompt("what is your name");
+// var age = prompt("what is your age");
+// var email= prompt("what is your email");
+// var password = prompt("what is your password");
+// console.log(name);
+// var score = 80;
+// var a = score >= 80 && score < 70;
+// console.log(a);
+// var score = 80;
+var score = 71;
+var a = score > 80 || score > 70;
+console.log(a);
