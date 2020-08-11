1dae8b7360e4f619fc2285c0d993ec3e8e9d0a5c
AbdulMoiz8994 <63612568+AbdulMoiz8994@users.noreply.github.com>
2020-08-11 10:50:09 +0500

    add index18.js

---
+++ index18.js
@@ -0,0 +1,90 @@
+// var fullName = "abdul moiz";
+// var age = 19;
+// var nationality = "Pakistani"
+// var state = "sindh";
+// isYouLoggedFromGoogle = true;
+// var country;
+// console.log(`
+//   Full Name = ${fullName}
+//   Age = ${age}
+//   Nationality = ${nationality}
+//   State = ${state}
+//   You have loggin = ${isYouLoggedFromGoogle}
+//   Country = ${country}
+// `)
+// var sellingPrice = 99;
+// var listingPrice = 499;
+// var discountPercent = ((listingPrice - sellingPrice) / listingPrice) *100;
+// console.log(discountPercent);
+// displayDiscountPrice = Math.round(discountPercent)
+// console.log(displayDiscountPrice+ "% off");
+// var a = 7;
+// var b= 17;
+// var c = a+b;
+// console.log(c);
+// var a = 7;
+// var b= 17;
+// var c = a-b;
+// console.log(c);
+// var a = 7;
+// var b= 17;
+// var c = a*b;
+// console.log(c);var a = 7;
+// var b= 17;
+// var c = a/b;
+// console.log(c);
+// displayInMathRule = Math.round(c);
+// console.log(displayInMathRule);
+// var a = 8;
+// var b = 5;
+// var c = a * (6 + (b / a)) - 14 + a;
+// console.log(a);
+// console.log(b);
+// console.log(c+ "%");
+// var a = 19;
+// var b = 45;
+// var c = (a-- + b++ - --a - a++ + --b);
+// console.log(a);18
+// console.log(b);45
+// console.log(c);75
+// var a = 8;
+// var b = 6;
+// var c = a>b && a>=b;
+// console.log(c); 
+// var a = 8;
+// var b = 6;
+// var c = a>b || a>=b;
+// console.log(c);
+// var a =10;
+// var b= 19;
+// console.log(a==b);
+// console.log(a===b);
+// console.log(a!=b);
+// console.log(a>b);
+// console.log(a<b);
+// console.log(a>=b);
+// console.log(a<=b);
+// var a = 20;
+// var b =40;
+// var c =19;
+// var d =!(a >c && b<a && c==a && b!=a);
+// console.log(d);
+// var d =!!(a >c && b<a && c==a && b!=a);
+// console.log(d);
+// var temperature = (prompt("what is the temperature"));
+// if(temperature >=-10  && temperature <= 25){
+//     console.log("It is too cold here");
+//     if(temperature >=-1){
+//         console.log("please stay at home, be safe");
+//     }
+// }
+// if(temperature >=40 && temperature <= 60){
+//     console.log("It is too warm");
+// }
+// else{
+//     console.log("It is normal");
+// }
+var age = 40;
+if(age>60){
+    console.log("If Age ="+age)
+}
