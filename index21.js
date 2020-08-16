dc9fc4a1ea0caa35dc396e2fe49c5d2ef1f27a8f
AbdulMoiz8994 <63612568+AbdulMoiz8994@users.noreply.github.com>
2020-08-16 13:02:48 +0500

    update index21.js

--- index21.js
+++ index21.js
@@ -52,10 +52,63 @@ var c =30;
 //     }
 //     console.log(number+" x "+a+" = "+(number*a));
 // }
-var num = parseInt(prompt("Please enter tabel"));
-for(var a=2;a<num;a++){
-    var result = num%a;
-    if(result==0){
-        console.log("This number is not prime "+num +" A / "+a);
-    }
-}
+// var num = parseInt(prompt("Please enter number"));
+// var isprime = true;
+// for(var a=2;a<num;a++){
+//     var result = num % a;
+//     if(result == 0){
+//         console.log("This number is not prime "+num +" A / "+a);
+//         isprime = false
+//         break;
+//     }
+//     // else{
+//     //     console.log("This is the prime nub "+num +" A /"+a);
+//     // }
+// }
+// if(isprime == true){
+//     console.log("this is  prime nub "+num);
+// }
+// var table =parseInt(prompt("what is your table"));
+// for(var a=1;a<=10;a++){
+//     console.log(table+" x "+a+" = "+(table*a));
+// }
+// Array Example
+
+// var food = ["pizza","Burger","Sandwich"];
+// // var food2 =  new Array["pizza","Burger","Sandwich"];
+// var a =food[0];
+// alert(a);
+// var b = food[1]
+// alert(b);
+// alert(food[2]);
+// var arr1 = [23,45,56,12,56];
+// var arr2 = [true,false,true,false,333];
+// var arr3 = [1,"abdul moiz",1111];
+// const arr4 = [{name: "Abdul",name: "Moiz"}]
+// console.log(arr1[2]);
+// console.log(arr2[4]);
+// console.log(arr3[1]);
+// console.log(arr4[0]);
+// var foods = ["pizza","Burger","Sandwitch"];
+// alert(foods);
+// console.log(foods);
+// console.log(foods[3]);
+// console.log(foods[4]);
+// we  will update things
+var foods = [];
+console.log(foods);
+foods [0] = "pizza";
+// foods [1] = "burger";
+// foods [2] = "sandwitch"
+foods [1] = "sandwitch"
+foods [2] = "frenchfries"
+console.log(foods[0]);
+console.log(foods[1]);
+console.log(foods[2]);
+console.log(foods);
+foods[1] = "burger";
+console.log(foods);
+console.log(foods[1]);
+// foods [7] = "PIZZA";
+// console.log(foods);
+
