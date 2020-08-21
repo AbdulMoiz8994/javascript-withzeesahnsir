709826f7c1b32ab5bc6c94b2b6b247e2a45166b8
AbdulMoiz8994 <63612568+AbdulMoiz8994@users.noreply.github.com>
2020-08-21 13:02:23 +0500

    add index25.js

---
+++ index25.js
@@ -0,0 +1,111 @@
+// var fullName = "abdul moiz";
+// var fatherName = "zafar Ali";
+// var nationality = "Pkaistani";
+// var state = "Sindh"
+// var motherTongue = "Urdu";
+// var age = 19;
+// var isLoggedInFromGoogle = true;
+// console.log(`
+//    Full Name = ${fullName}
+//    Father Name = ${fatherName}
+//    Nationality = ${nationality}
+//    State = ${state}
+//    Mother Tongue = ${motherTongue}
+//    Age = ${age}
+//    Is You Loggin = ${true}
+// `)
+// var sellingprice = 499;
+// var listingprice = 715;
+// var discountprice = (listingprice - sellingprice) / listingprice *100;
+// console.log(discountprice);
+// var totaldiscount = Math.round(discountprice);
+// console.log(totaldiscount);
+// var a = 17;
+// var b =18;
+// var c = a++ - --b + --a;
+// console.log(a);
+// console.log(b);
+// console.log(c); 
+// var a = "hello "+3+4;
+// var b = 5+10+" hello";
+// var c = "hello "+"5"+8;
+// console.log(a); 
+// console.log(b); 
+// console.log(c); 
+// var age = 20;
+// var age = prompt("what is your age");
+// console.log(age); 
+// var nub = parseInt(prompt("what is your fav nub"));
+// console.log(nub);
+// var nub = parseFloat(prompt("what is your fav nub"));
+// console.log(nub);
+// var nub = Number(prompt("Please type nub"));
+// console.log(nub);
+// var a = prompt("This is your pop up");
+// alert("hello world");
+// var a = 80;
+// var b = a==80;
+// var c = a===80;
+// var d = a!=80;
+// var e = a>=80;
+// var f = a<=80;
+// var g = a>80;
+// var h = a<80;
+// console.log(b);
+// console.log(c);
+// console.log(d);
+// console.log(e);
+// console.log(f);
+// console.log(g);
+// console.log(h);
+// var a =10;
+// var b=20;
+// var c= a>b && b<30;
+// console.log(c);
+// var d = a<b && (b<40 || b==a);
+// console.log(d);
+// var d = a<b && (b>40 || b==a);
+// console.log(d)
+// var grade =prompt("what is your grade");
+// if(grade>=80 && grade<=99){
+//     console.log("GRADE A1");
+//     if(grade>=90){
+//         console.log("Reward $50");
+//     }
+//     else if(grade>=85){
+//         console.log("Reward $30");
+//     }
+// }
+// else if(grade>=70 && grade<=79.99){
+//     console.log("Grade A");
+// }
+// else{
+//     console.log("Bad");
+// }
+// for(var a=1;a<10;a++){
+//     console.log(a);
+// }
+// var table=parseInt(prompt("which is fav table"))
+// for(var a=1;a<=10;a++){
+//     console.log(table+" x "+a+" = "+(table*a));
+// }
+// var a = [1,2,3,4,6,7];
+// console.log(a);
+// console.log(a[3]);
+// var table = ["abdul moiz","abdul rafay","abdul malik"];
+// console.log(table);
+// console.log(table[0]);
+// table[0]="konain ali";
+// console.log(table);
+var listOfName =[];
+listOfName.push("moiz");
+listOfName.push("rafay");
+listOfName.push("ali");
+listOfName.push("whadan");
+listOfName.push("abdullah");
+listOfName.push("faizan");
+console.log(listOfName);
+console.log(listOfName[1]);
+listOfName[1]="Ali khanzada";
+console.log(listOfName);
+console.log(listOfName.length);
