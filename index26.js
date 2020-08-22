f61b4ea5135d0cddbe19aa47e85435fbaf65e0f8
AbdulMoiz8994 <63612568+AbdulMoiz8994@users.noreply.github.com>
2020-08-22 11:59:22 +0500

    add index26.js

---
+++ index26.js
@@ -0,0 +1,33 @@
+var a=[1,2,3,4,5,6,7,8];
+console.log(a[3]);
+console.log(a);
+a[0]=(34);
+console.log(a);
+// var names = [];
+// names.push(prompt("what is your first fav friend"));
+// names.push(prompt("what is your second fav friend"));
+// names.push(prompt("what is your third fav friend"));
+// console.log(names);
+// console.log(names[1]);
+// names.push("abdul moiz");
+// names.push("konain ali");
+// names.push("abdul rafay","basit");
+// names.push("abdul malik");
+// names.push("abdullah");
+// names.push("faizan");
+// console.log(names);
+// alert(names[0]);
+// console.log(names.length);
+// console.log(names[-1]);
+// nested loops
+// for(var a=0;a<2;a++){
+//     for(var b=0;b<2;b++){
+//         console.log("A = "+a+ " B = "+b)
+//     }
+// }
+var num = prompt("what is your nub");
+for(var a=0;a<num;a++){
+    for(var b=0;b<num;b++){
+        console.log("A = "+a+ " B = "+b);
+    }
+}
