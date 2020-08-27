f4456181195dc2a10b20534fdba813d6f031b5a5
AbdulMoiz8994 <63612568+AbdulMoiz8994@users.noreply.github.com>
2020-08-27 15:31:56 +0500

    index28.js

---
+++ index28.js
@@ -0,0 +1,51 @@
+var fullName = "abdul moiz";
+var fatherName = "zafar ali";
+var nationality = "Pakistan";
+var email = "moiza88994@gmail.com";
+var collageName = "Asam Collage";
+var age = 20;
+var isYouLogginFromGoogle = true;
+var state = "sindh";
+console.log(`
+   Full Name = ${fullName};
+   Father Name = ${fatherName};
+   Nationality = ${nationality};
+   Email = ${email};
+   Collage Name = ${collageName};
+   Age = ${age};
+   Is You Loggin = ${isYouLogginFromGoogle};
+   State = ${state};
+`)
+// var arr1 = ["abdul moiz","zafar ali","abdul Rafay","abdul Rafay"];
+// console.log(arr1);
+// arr1.push("rizwan");
+// arr1.push("Abdullah");
+// console.log(arr1);
+// console.log("Length "+arr1.length);
+// arr1.unshift("Faizan");
+// arr1.unshift("Whadan","Zeeshan");
+// console.log(arr1);
+// console.log(arr1[0]);
+// var a= arr1.pop();
+// console.log(a);
+// console.log(arr1);
+// console.log(arr1.length);
+// var b = arr1.unshift("khanzada123");
+// console.log(b);
+// console.log(arr1[0]);
+// var c = arr1.shift();
+// console.log(c);
+// var d = arr1.shift();
+// console.log(d);
+// console.log(arr1);
+// var foods = ["Pizza","Burger","Snacks","Sandwich"];
+// for(var a=0;a<foods.length;a++){
+//     console.log(foods[a]);
+// }
+var arr1 = [4,6,8,10,12];
+for(var a=0;a<arr1.length;a++){
+    arr1[a] = arr1[a]*2;
+}
+for(var b =0;b<arr1.length;b++){
+    console.log(arr1[b]);
+}
