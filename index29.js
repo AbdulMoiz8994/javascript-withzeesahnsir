0102a175c849b9f1bf2204736e4cca67e8493ca9
AbdulMoiz8994 <63612568+AbdulMoiz8994@users.noreply.github.com>
2020-09-01 22:23:09 +0500

    update index29.js

--- index29.js
+++ index29.js
@@ -1,33 +1,46 @@
-// // // var a = ["pizza","Mango","Orange","sandwich","pizza"];
-// // // console.log(a);
-// // // a.splice(1,0,"fries");
-// // // console.log(a);
-// // // a.splice(3,1,"snakes");
-// // // console.log(a);
-// // // var b=a.lastIndexOf("pizza");
+// // // // var a = ["pizza","Mango","Orange","sandwich","pizza"];
+// // // // console.log(a);
+// // // // a.splice(1,0,"fries");
+// // // // console.log(a);
+// // // // a.splice(3,1,"snakes");
+// // // // console.log(a);
+// // // // var b=a.lastIndexOf("pizza");
+// // // // console.log(b);
+// // // // // string
+// // // var a = "hello abdul moiz";
+// // // console.log(a.length);
+// // // var b = "hello\"world";
 // // // console.log(b);
-// // // // string
-// // var a = "hello abdul moiz";
-// // console.log(a.length);
-// // var b = "hello\"world";
-// // console.log(b);
-// // var c = "hello\\world";
-// // console.log(c);
-// // var d = "hello\nworld";
-// // console.log(d);
-// // var e = "hello\t\tmoiz";
-// // console.log(e);
-// var names = "abdul MOIZ";
-// var updatename = names.toLocaleLowerCase();
-// console.log(names);
-// console.log(updatename);
-var food = "Sandwich";
-var input = prompt("write your favourite food");
-console.log("user input = "+input);
-if(food.toLocaleLowerCase() === input.toLocaleLowerCase()){
-    console.log("Match");
-}
-else{
-    console.log("Do Not Match");
-}
+// // // var c = "hello\\world";
+// // // console.log(c);
+// // // var d = "hello\nworld";
+// // // console.log(d);
+// // // var e = "hello\t\tmoiz";
+// // // console.log(e);
+// // var names = "abdul MOIZ";
+// // var updatename = names.toLocaleLowerCase();
+// // console.log(names);
+// // console.log(updatename);
+// // var food = ("Sandwich","piZZa");
+// // var input = prompt("write your favourite food");
+// // console.log("user input = "+input);
+// // if(food.toLocaleLowerCase() === input.toLocaleLowerCase()){
+// //     console.log("Match");
+// // }
+// // else{
+// //     console.log("Do Not Match");
+// // }
+// var a = ("khanzada123 moiz abdul");
+// console.log(a);
+// var b =a.slice(3);
+// console.log(b);
+// console.log(a.indexOf("moiz"));
+// console.log(a.indexOf("zada"));
+// console.log(a.indexOf("bdul"));
+// console.log(a.slice(-9,-1));
+var a = "to be or not To be";
+var b = a.indexOf("not");
+console.log(b);
+var b = a.lastIndexOf("be");
+console.log(b);
 
