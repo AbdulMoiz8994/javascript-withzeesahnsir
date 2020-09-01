d0b265f13f02f0c66883c3c9321c93a4c8bcc018
AbdulMoiz8994 <63612568+AbdulMoiz8994@users.noreply.github.com>
2020-09-01 18:38:05 +0500

    update index29.js

--- index29.js
+++ index29.js
@@ -1,19 +1,33 @@
-// var a = ["pizza","Mango","Orange","sandwich","pizza"];
-// console.log(a);
-// a.splice(1,0,"fries");
-// console.log(a);
-// a.splice(3,1,"snakes");
-// console.log(a);
-// var b=a.lastIndexOf("pizza");
-// console.log(b);
-// // string
-var a = "hello abdul moiz";
-console.log(a.length);
-var b = "hello\"world";
-console.log(b);
-var c = "hello\\world";
-console.log(c);
-var d = "hello\nworld";
-console.log(d);
-var e = "hello\t\tmoiz";
-console.log(e);
+// // // var a = ["pizza","Mango","Orange","sandwich","pizza"];
+// // // console.log(a);
+// // // a.splice(1,0,"fries");
+// // // console.log(a);
+// // // a.splice(3,1,"snakes");
+// // // console.log(a);
+// // // var b=a.lastIndexOf("pizza");
+// // // console.log(b);
+// // // // string
+// // var a = "hello abdul moiz";
+// // console.log(a.length);
+// // var b = "hello\"world";
+// // console.log(b);
+// // var c = "hello\\world";
+// // console.log(c);
+// // var d = "hello\nworld";
+// // console.log(d);
+// // var e = "hello\t\tmoiz";
+// // console.log(e);
+// var names = "abdul MOIZ";
+// var updatename = names.toLocaleLowerCase();
+// console.log(names);
+// console.log(updatename);
+var food = "Sandwich";
+var input = prompt("write your favourite food");
+console.log("user input = "+input);
+if(food.toLocaleLowerCase() === input.toLocaleLowerCase()){
+    console.log("Match");
+}
+else{
+    console.log("Do Not Match");
+}
+
