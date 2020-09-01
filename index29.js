f29402190c8b1a7a16c71c6e9d711df0d8bf54d3
AbdulMoiz8994 <63612568+AbdulMoiz8994@users.noreply.github.com>
2020-09-01 18:09:29 +0500

    update index29.js

---
+++ index29.js
@@ -0,0 +1,19 @@
+// var a = ["pizza","Mango","Orange","sandwich","pizza"];
+// console.log(a);
+// a.splice(1,0,"fries");
+// console.log(a);
+// a.splice(3,1,"snakes");
+// console.log(a);
+// var b=a.lastIndexOf("pizza");
+// console.log(b);
+// // string
+var a = "hello abdul moiz";
+console.log(a.length);
+var b = "hello\"world";
+console.log(b);
+var c = "hello\\world";
+console.log(c);
+var d = "hello\nworld";
+console.log(d);
+var e = "hello\t\tmoiz";
+console.log(e);
