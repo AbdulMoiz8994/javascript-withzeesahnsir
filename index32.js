4a76ef5e539572dcacd73b642cd18f79b505ed64
AbdulMoiz8994 <63612568+AbdulMoiz8994@users.noreply.github.com>
2020-09-05 12:30:15 +0500

    add index32.js

---
+++ index32.js
@@ -0,0 +1,31 @@
+// math 
+var sellingPrice = 499;
+var listingPrice = 999;
+var discount = (listingPrice-sellingPrice)/listingPrice*100;
+console.log(discount);
+console.log(discount.toFixed(1));
+totalDiscount = Math.round(discount)
+console.log(totalDiscount+"% off");
+var math = Math.random(1);
+console.log(math);
+console.log(math.toFixed(2));
+console.log(Math.ceil(math));
+console.log(Math.floor(math));
+var num = math*6;
+console.log(num);
+var num1 = num+1;
+console.log(num1);
+console.log(num1.toFixed(2));
+console.log(Math.floor(num1));
+var date = new Date();
+console.log(date);
+var date1 = new Date(2020,10,08,12,05,40,900);
+console.log(date1);
+var date2 = new Date("January/10/2020 10:05:30")
+console.log(date2);
+var date3 = new Date();
+var date4= date3.getTime();
+console.log(date4);
+var date5 = new Date(0);
+console.log(date5);
+
