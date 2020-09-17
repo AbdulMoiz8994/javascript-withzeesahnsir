ce19f2020e4c243853791bd7a82012aee3c28af6
AbdulMoiz8994 <63612568+AbdulMoiz8994@users.noreply.github.com>
2020-09-17 10:45:04 +0500

    update index36.js

---
+++ index36.js
@@ -0,0 +1,77 @@
+// // // // var name = "Abdul moiz";
+// // // // var fatherName = "zafar ali";
+// // // // var nationality = "Pakistani";
+// // // // var state = "Sindh";
+// // // // var age = 19;
+// // // // var email = "moiza8994@gmail.com";
+// // // // var feesPaid = true;
+// // // // console.log(`
+// // // //         Name = ${name}
+// // // //         Father Name = ${fatherName}
+// // // //         Nationality = ${nationality}
+// // // //         State = ${state}
+// // // //         Age = ${age}
+// // // //         Email = ${email}
+// // // //         Fees Paid = ${feesPaid}
+// // // // `)
+// // // // function name1(nam1,nam2){
+// // // //        console.log("hello "+nam1,nam2)
+// // // // }
+// // // // name1("abdul moiz","Attari"); 
+// // // // name1("Mudasir Raza","Attari"); 
+// // // // name1("Jameeel Raza","Attari"); 
+// // // // name1("Amjad Raza","Attari"); 
+// // // // name1("Sohail","Attari");
+// // // // name1("Shareef","Attari");  
+// // // // function pract(a, b){
+// // // //     return(a+b)
+// // // // }
+// // // // // var a =pract(23,72);
+// // // // // console.log(a);
+// // // // function grade(grade){
+// // // // if(grade >=80 && grade<=99.99){
+// // // //       console.log("Grade A")
+// // // //   }
+// // // // else{
+// // // //     console.log("Fail");
+// // // // }
+// // // // }
+// // // // var a = parseInt(prompt("What is your score"));
+// // // // console.log(a);
+// // // // grade(a);
+// // // function multipy(num1,num2){
+// // //     return num1*num2
+// // // }
+// // // var a =multipy(45,87)+20;
+// // // console.log(a);
+// // function multipy(num1,num2){
+// //     return num1*num2
+// // }
+// // function sum(a,b){
+// //     return multipy(a,b)+a
+// // }
+// // var add = sum(20,20)+10; 
+// // console.log(add);
+// // // console.log(sum(10,10)+6);
+// function multipy(nub1,nub2){
+//     return nub1*nub2;
+// }
+// function sum(a,b){
+//     return a+b;
+// }
+// var total = sum(multipy(7,8),2)+6;
+// console.log(total);
+function multipy(nub1,nub2){
+    return nub1*nub2
+}
+function sum(a,b){
+    return multipy(a,b)+b
+}
+var add  =sum(5,19)
+console.log("addition = "+add);
+// var mul = multipy(20,9);
+// var add = sum(mul,9);
+// console.log(mul);
+// console.log(add);
+// var add= sum(multipy(30,9),5);
+// console.log("addition = "+add);
