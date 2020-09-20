128f1723f9578d50620843d064bd04a1a2365e0d
AbdulMoiz8994 <63612568+AbdulMoiz8994@users.noreply.github.com>
2020-09-20 16:44:06 +0500

    update index38.js

--- index38.js
+++ index38.js
@@ -1,67 +1,90 @@
-// // // // var total = sum(4,5)
-// // // // console.log(total);
-// // // // var mul = seq(9);
-// // // // console.log(mul);
+// // // // // // var total = sum(4,5)
+// // // // // // console.log(total);
+// // // // // // var mul = seq(9);
+// // // // // // console.log(mul);
 
-// // // // function sum(a,b){
-// // // //     return a+b;
-// // // // }
-// // // // function seq(num){
-// // // //     return num*num
-// // // // }
-// // // // console.log(a);
-// // // // var a=6;
-// // // // console.log(a);
-// // // var mul= multiply(10,7);
-// // // // var add = sum(10,7);
-// // // console.log(mul);
-// // // // console.log(add);
-// // // console.log(b);
-// // // function multiply(num1,num2){
-// // //     return num1*num2
-// // // }
-// // // var b=15;
-// // // var sum = function(a,b){
-// // //     return a-b;
-// // // }
-// var num = 5;
-// function sum(a){
-//      a=7;
-//      console.log(a);
-// }
-// sum(num);
-// console.log(num);
-
-
-// // var a = [15,3,5,9,1];
-// // function update(val){
-// //     val[2]=(555)
+// // // // // // function sum(a,b){
+// // // // // //     return a+b;
+// // // // // // }
+// // // // // // function seq(num){
+// // // // // //     return num*num
+// // // // // // }
+// // // // // // console.log(a);
+// // // // // // var a=6;
+// // // // // // console.log(a);
+// // // // // var mul= multiply(10,7);
+// // // // // // var add = sum(10,7);
+// // // // // console.log(mul);
+// // // // // // console.log(add);
+// // // // // console.log(b);
+// // // // // function multiply(num1,num2){
+// // // // //     return num1*num2
+// // // // // }
+// // // // // var b=15;
+// // // // // var sum = function(a,b){
+// // // // //     return a-b;
+// // // // // }
+// // var num = "abdul moiz";
+// // function sum(a){
+// //      a=7;
+// //      console.log(a);
 // // }
-// // console.log(a);
-// // update(a);
-// // console.log(a);
-// // console.log(a[2]);
-// var obj ={ name:"abdul moiz", age:50};
-// function updateAge(a){
-//     a.age=40;
-//     a.name="abdul rafay"
-// }
-// console.log(obj);
-// updateAge(obj);
-// console.log(obj);
-// var a = 70;
-// function test(num){
-//     num=50;
-//     console.log(num);
-// }
+// // sum(num);
 // // console.log(num);
-// test(a);
-// console.log(a);
 
-var b = [24,56,78,7,6];
-var sum = function(arr){
-    console.log(arr);
-    arr[1]=(100);
-}
-console.log(b);
-sum(b);
+
+// // // var a = [15,3,5,9,1,];
+// // // function update(val){
+// // //     val[2]=(674)
+// // // }
+// // // console.log(a);
+// // // update(a);
+// // // console.log(a);
+// // // // console.log(a[2]);
+// // // var obj ={ name:"abdul moiz", age:50};
+// // // function updateAge(a){
+// // //     a.age=40;
+// // //     a.name="abdul rafay"
+// // // }
+// // // console.log(obj);
+// // // updateAge(obj);
+// // // console.log(obj);
+// // // var a = 70;
+// // // function test(num){
+// // //     num=50;
+// // //     console.log(num);
+// // // }
+// // // // console.log(num);
+// // // test(a);
+// // // console.log(a);
+
+// // // var b = [24,56,78,7,6,99];
+// // // function sum(arr){
+// // //     console.log(arr);
+// // //     arr=[200,300,400,500];
+// // //     arr[2]=(1000);
+// // //     console.log(arr);
+// // // }
+// // // sum(b);
+// // // console.log(b);
+// // function factorail(num){
+// //     if(num<=1){
+// //     return 1;
+// // }
+// // else{
+// //     return num*factorail(num-1);
+// // }
+// // }
+// // console.log(factorail(6));
+// function nub(sum){
+//     if(sum<=1){
+//         return 2;
+//     }
+//     else{
+//         return sum*nub(sum-2)
+//     }
+// }
+// //6*16(6-2)
+// //4*4(4-2)
+// //2*2(2-2)
+// console.log(nub(6));
