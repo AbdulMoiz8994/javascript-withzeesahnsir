03850b697b6fb4c92050fe1242bf2d94d6a68d6a
AbdulMoiz8994 <63612568+AbdulMoiz8994@users.noreply.github.com>
2020-09-22 11:18:48 +0500

    update index39.js

--- learn bootstrap/index39.js
+++ learn bootstrap/index39.js
@@ -1,135 +1,153 @@
-// // // // // // // // var date1 = new Date("June,04,2020 14:04:30");
-// // // // // // // // var date2 = new Date("June,14,2020 14:04:30");
-// // // // // // // // console.log(date1);
-// // // // // // // // console.log(date2);
-// // // // // // // // var totalDiff = date2.getTime()-date1.getTime();
-// // // // // // // // console.log(totalDiff);
-// // // // // // // // var dayTotalTime=(1000*60*60*24);
-// // // // // // // // console.log(dayTotalTime);
-// // // // // // // // console.log(totalDiff/dayTotalTime+" Days Remaining for this big oppertunity");
-// // // // // // // function name(message){
-// // // // // // //     console.log("hello "+message);
-// // // // // // // }
-// // // // // // // name("abdul moiz");
-// // // // // // // // console.log(showMessage());
-// // // // // // // // function showMessage(){
-// // // // // // // //     console.log("hello moiz");
+// // // // // // // // // var date1 = new Date("June,04,2020 14:04:30");
+// // // // // // // // // var date2 = new Date("June,14,2020 14:04:30");
+// // // // // // // // // console.log(date1);
+// // // // // // // // // console.log(date2);
+// // // // // // // // // var totalDiff = date2.getTime()-date1.getTime();
+// // // // // // // // // console.log(totalDiff);
+// // // // // // // // // var dayTotalTime=(1000*60*60*24);
+// // // // // // // // // console.log(dayTotalTime);
+// // // // // // // // // console.log(totalDiff/dayTotalTime+" Days Remaining for this big oppertunity");
+// // // // // // // // function name(message){
+// // // // // // // //     console.log("hello "+message);
 // // // // // // // // }
-// // // // // // // // showMessage();
-// // // // // // // // function mul(num1,num2,num3){
-// // // // // // // //     console.log(num1*num2*num3);
+// // // // // // // // name("abdul moiz");
+// // // // // // // // // console.log(showMessage());
+// // // // // // // // // function showMessage(){
+// // // // // // // // //     console.log("hello moiz");
+// // // // // // // // // }
+// // // // // // // // // showMessage();
+// // // // // // // // // function mul(num1,num2,num3){
+// // // // // // // // //     console.log(num1*num2*num3);
+// // // // // // // // // }
+// // // // // // // // // mul(3,5,7);
+// // // // // // // // // mul(10,82,89,81);
+// // // // // // // // function nam(name,nub){
+// // // // // // // //     console.log("hello "+name,nub)
 // // // // // // // // }
-// // // // // // // // mul(3,5,7);
-// // // // // // // // mul(10,82,89,81);
-// // // // // // // function nam(name,nub){
-// // // // // // //     console.log("hello "+name,nub)
+// // // // // // // // nam("abdul moiz");
+// // // // // // // // nam("zafar ali");
+// // // // // // // // nam(true);
+// // // // // // // // nam(45);
+// // // // // // // // nam();
+// // // // // // // // nam("moiz",12);
+// // // // // // // function sum(a,b){
+// // // // // // //     console.log(a+b);
 // // // // // // // }
-// // // // // // // nam("abdul moiz");
-// // // // // // // nam("zafar ali");
-// // // // // // // nam(true);
-// // // // // // // nam(45);
-// // // // // // // nam();
-// // // // // // // nam("moiz",12);
-// // // // // // function sum(a,b){
-// // // // // //     console.log(a+b);
+// // // // // // // sum(4,6);
+// // // // // // // sum(14);
+// // // // // // // function mul(num1,num2){
+// // // // // // //     console.log(num1*num2);
+// // // // // // // }
+// // // // // // // mul(10,5);
+// // // // // // // mul(34,6,7);
+// // // // // // // mul("abdul moiz")
+// // // // // // function finalGrade(score){
+// // // // // //     if(score>=80 && score<=100){
+// // // // // //         console.log("Grade A1")
+// // // // // //     }
+// // // // // //     else if(score>=70 && score<=79.99){
+// // // // // //         console.log("Grade A")
+// // // // // //     }
+// // // // // //     if(score>=60 && score<=69.99){
+// // // // // //         console.log("Grade B")
+// // // // // //     }
+// // // // // //     if(score>=55 && score<=59.99){
+// // // // // //         console.log("Grade C")
+// // // // // //     }
+// // // // // //     if(score>=50 && score<=54.99){
+// // // // // //         console.log("Grade D")
+// // // // // //     }
+// // // // // //     else{
+// // // // // //         console.log("Fail")
+// // // // // //     }
 // // // // // // }
-// // // // // // sum(4,6);
-// // // // // // sum(14);
-// // // // // // function mul(num1,num2){
-// // // // // //     console.log(num1*num2);
-// // // // // // }
-// // // // // // mul(10,5);
-// // // // // // mul(34,6,7);
-// // // // // // mul("abdul moiz")
-// // // // // function finalGrade(score){
-// // // // //     if(score>=80 && score<=100){
-// // // // //         console.log("Grade A1")
-// // // // //     }
-// // // // //     else if(score>=70 && score<=79.99){
-// // // // //         console.log("Grade A")
-// // // // //     }
-// // // // //     if(score>=60 && score<=69.99){
-// // // // //         console.log("Grade B")
-// // // // //     }
-// // // // //     if(score>=55 && score<=59.99){
-// // // // //         console.log("Grade C")
-// // // // //     }
-// // // // //     if(score>=50 && score<=54.99){
-// // // // //         console.log("Grade D")
-// // // // //     }
-// // // // //     else{
-// // // // //         console.log("Fail")
-// // // // //     }
+// // // // // // finalGrade(78);
+// // // // // // finalGrade(45);
+// // // // // // finalGrade(87);
+// // // // // // finalGrade(55);
+// // // // // // finalGrade(67);
+// // // // // // function num(){
+// // // // // //     return 56;
+// // // // // // } 
+// // // // // // var a =num();
+// // // // // // console.log(a);
+// // // // // // function sum(){
+// // // // // //     return 67;
+// // // // // //     return 56;
+// // // // // // } 
+// // // // // // var a =sum();
+// // // // // // console.log(a);
+
+// // // // // // var num = sum();
+// // // // // // console.log(num);
+// // // // // var plus=mul(41,56,67);
+// // // // // console.log(plus);
+// // // // // console.log()
+// // // // // var sum = function(a,b){
+// // // // //     return a+b
 // // // // // }
-// // // // // finalGrade(78);
-// // // // // finalGrade(45);
-// // // // // finalGrade(87);
-// // // // // finalGrade(55);
-// // // // // finalGrade(67);
-// // // // // function num(){
-// // // // //     return 56;
-// // // // // } 
-// // // // // var a =num();
-// // // // // console.log(a);
-// // // // // function sum(){
-// // // // //     return 67;
-// // // // //     return 56;
-// // // // // } 
-// // // // // var a =sum();
-// // // // // console.log(a);
-
-// // // // // var num = sum();
-// // // // // console.log(num);
-// // // // var plus=mul(41,56,67);
-// // // // console.log(plus);
-// // // // console.log()
-// // // // var sum = function(a,b){
-// // // //     return a+b
+// // // // // function mul(num1,num2,num3){
+// // // // //     return num1*num3*num2-num1
+// // // // // }
+// // // // var obj={name:"abdul malik",age:19,nationality:"Pakistani"}
+// // // // function realObj(a){
+// // // //      a.name="abdul rafay"
+// // // //      a.age=20
 // // // // }
-// // // // function mul(num1,num2,num3){
-// // // //     return num1*num3*num2-num1
-// // // // }
-// // // var obj={name:"abdul malik",age:19,nationality:"Pakistani"}
-// // // function realObj(a){
-// // //      a.name="abdul rafay"
-// // //      a.age=20
+// // // // console.log(obj);
+// // // // realObj(obj);
+// // // // console.log(obj);
+// // // var a="abdul moiz";
+// // // function nam(b){
+// // //     b="abdul malik"
+// // //     console.log(b);
 // // // }
-// // // console.log(obj);
-// // // realObj(obj);
-// // // console.log(obj);
-// // var a="abdul moiz";
-// // function nam(b){
-// //     b="abdul malik"
-// //     console.log(b);
-// // }
-// // console.log(a);
-// // nam(a);
-// // console.log(a);
-// nam();
-// console.log(b);
-
-// function nam(){
-//     var a=20;
-//     console.log(a);
-//      return b=10;
-// }
-
+// // // console.log(a);
+// // // nam(a);
+// // // console.log(a);
+// // nam();
 // // console.log(b);
-// var arr=[23,45,64];
-// function arry(b){
-//     console.log(b);
-//     b[2]=(1000)
-//     console.log(b)
+
+// // function nam(){
+// //     var a=20;
+// //     console.log(a);
+// //      return b=10;
+// // }
+// // // console.log(b);
+// // var arr=[23,45,64];
+// // function arry(b){
+// //     console.log(b);
+// //     b[2]=(1000)
+// //     console.log(b)
+// // }
+// // arry(arr);
+// // // console.log(arr);
+// // var arr=[23,45,64];
+// // function arry(b){
+// //     console.log(b);
+// //     b=[22,50,50]
+// //     console.log(b)
+// // }
+// // // arry();
+// // // console.log(b);
+// // arry(arr);
+// // console.log(arr);
+// function factorial(num){
+//     if(num<=2){
+//         return 1;
+//     }
+//     else{
+//         return num*factorial(num-1)
+//     }
 // }
-// arry(arr);
-// console.log(arr);
-var arr=[23,45,64];
-function arry(b){
-    console.log(b);
-    b=[22,50,50]
-    console.log(b)
+// console.log(factorial(7));
+function nub(des){
+    if(des<2){
+        return 3;
+    }
+    else{
+        return des*nub(des-2)
+    }
 }
-// arry();
-console.log(b);
-arry(arr);
-console.log(arr);
+var a = nub(12);
+console.log(a);
