6fe706a3b13ed9b3f22e5e8aa274b52ad3d1e98d
AbdulMoiz8994 <63612568+AbdulMoiz8994@users.noreply.github.com>
2020-09-24 11:44:43 +0500

    add index40.js

---
+++ learn bootstrap/index40.js
@@ -0,0 +1,67 @@
+// // // var day =parseInt(prompt("Plese Type nub"));
+// // // switch (day) {
+// // //     case 6:
+// // //         console.log("Today is Weekend Night")
+// // //         break;
+// // //         case 0:
+// // //             console.log("Today is Sunday")
+// // //             break;
+// // //      case 5:
+// // //      console.log("Today is Friday");
+// // //       break;
+// // //       case 4:
+// // //         console.log("Today is Thrusday");
+// // //         break;
+// // //     default:
+// // //         console.log("oh No Today is working day")
+// // //         break;
+// // // }
+// // // var day=parseInt(prompt("please type number"));
+// // // switch (day) {
+// // //     case 5:
+// // //     case 1:    
+// // //     console.log("It is a week day");
+// // //         break;
+// // //     default:
+// // //         console.log("Looking Forward to the weekend");
+// // //         break;
+// // // }
+// // var day = "1";
+// // switch (day) {
+// //     case 1:
+// //         console.log("Today is Monday");
+// //         break;
+// //         case 0:
+// //             console.log("Today is Weekend");
+// //             break;
+// //     default:
+// //         console.log("Again working day")
+// //         break;
+// // }
+// // var i=parseInt(prompt("Please type numb"));
+// // while(i<=10){
+// //     console.log("I ="+i);
+// //     i++;
+// // }
+// var docontinue= true;
+// while(docontinue){
+//     console.log("Heloo world")
+
+//     var input=prompt("If User Press Y then carry on any other key the stop");
+//     if(input.toLowerCase() !== "y"){
+//           docontinue=false;        
+//     }
+// }
+// console.log("while finish");
+// var i=10;
+// do{
+//     console.log("I = "+i)
+//     i++;
+// }
+// while(i<11)
+var i=10;
+do{
+    console.log("I = "+i)
+    i++;
+}
+while(i<10)
